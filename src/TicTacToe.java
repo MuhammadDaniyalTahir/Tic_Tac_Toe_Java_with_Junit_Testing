a3e9ccf0d29373fccc8e3f79f2858b97bb63d9ce
Muhammad Daniyal Tahir <muhammaddaniyaltahir969@gmail.com>
2023-12-09 10:51:11 +0500

    Implementation of function playGame has been done.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,7 +9,7 @@ public class TicTacToe {
         playGame();
     }
 
-    private static void displayBoard() {
+    static void displayBoard() {
         System.out.println("  0 1 2");
         for (int i = 0; i < 3; i++) {
             System.out.print(i + " ");
@@ -26,15 +26,41 @@ public class TicTacToe {
         }
     }
 
-    private static void playGame() {
+    static void playGame() {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
 
+        while (true) {
+            System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+
+            if (isValidMove(row, col)) {
+                board[row][col] = currentPlayer;
+                displayBoard();
+
+                if (checkWin()) {
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    break;
+                } else if (isBoardFull()) {
+                    System.out.println("The game is a tie!");
+                    break;
+                }
+
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+
+        scanner.close();
     }
 
-    private static boolean isValidMove(int row, int col) {
+    static boolean isValidMove(int row, int col) {
         return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ';
     }
 
-    private static boolean checkWin() {
+    static boolean checkWin() {
         // Check rows, columns, and diagonals for a win
         for (int i = 0; i < 3; i++) {
             if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
@@ -52,7 +78,7 @@ public class TicTacToe {
         return board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer; // Check diagonal (top-right to bottom-left)
     }
 
-    private static boolean isBoardFull() {
+    static boolean isBoardFull() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] == ' ') {
