322de72d27c42cd2514afaa6c9889af1ff21e0fe
Muhammad Daniyal Tahir <muhammaddaniyaltahir969@gmail.com>
2023-12-09 10:36:44 +0500

    Structure of functions have made.

---
+++ src/TicTacToe.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        displayBoard();
+        playGame();
+    }
+
+    private static void displayBoard() {
+
+    }
+
+    private static void playGame() {
+
+    }
+
+    private static boolean isValidMove(int row, int col) {
+        return true;
+    }
+
+    private static boolean checkWin() {
+
+    }
+
+    private static boolean isBoardFull() {
+
+        return true;
+    }
+}
