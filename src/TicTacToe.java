765582ca66f8160e4d2f178bed612a5e09ef98f2
Muhammad Daniyal Tahir <muhammaddaniyaltahir969@gmail.com>
2023-12-09 10:41:48 +0500

    Implementation of displaying board function has been done.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -10,7 +10,20 @@ public class TicTacToe {
     }
 
     private static void displayBoard() {
-
+        System.out.println("  0 1 2");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+            if (i < 2) {
+                System.out.println("  -----");
+            }
+        }
     }
 
     private static void playGame() {
@@ -22,7 +35,7 @@ public class TicTacToe {
     }
 
     private static boolean checkWin() {
-
+        return true;
     }
 
     private static boolean isBoardFull() {
